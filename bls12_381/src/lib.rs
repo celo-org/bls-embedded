46c8dafa2b54ada34c72b9a4768fa494175045e9
Michael Straka <mstraka100@gmail.com>
2019-09-05 12:25:01 -0700

    added bls381 files

---
+++ bls12_381/src/lib.rs
@@ -0,0 +1,91 @@
+//! # `bls12_381`
+//!
+//! This crate provides an implementation of the BLS12-381 pairing-friendly elliptic
+//! curve construction.
+//!
+//! * **This implementation has not been reviewed or audited. Use at your own risk.**
+//! * This implementation targets Rust `1.36` or later.
+//! * This implementation does not require the Rust standard library.
+//! * All operations are constant time unless explicitly noted.
+
+#![no_std]
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
+#![allow(clippy::too_many_arguments)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::many_single_char_names)]
+// This lint is described at
+// https://rust-lang.github.io/rust-clippy/master/index.html#suspicious_arithmetic_impl
+// In our library, some of the arithmetic involving extension fields will necessarily
+// involve various binary operators, and so this lint is triggered unnecessarily.
+#![allow(clippy::suspicious_arithmetic_impl)]
+
+#[cfg(feature = "pairings")]
+extern crate alloc;
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+#[cfg(test)]
+#[cfg(feature = "groups")]
+mod tests;
+
+#[macro_use]
+mod util;
+
+/// Notes about how the BLS12-381 elliptic curve is designed, specified
+/// and implemented by this library.
+pub mod notes {
+    pub mod design;
+}
+
+mod scalar;
+
+pub use scalar::Scalar;
+
+#[cfg(feature = "groups")]
+mod fp;
+#[cfg(feature = "groups")]
+mod fp2;
+#[cfg(feature = "groups")]
+mod g1;
+#[cfg(feature = "groups")]
+mod g2;
+
+#[cfg(feature = "groups")]
+pub use g1::{G1Affine, G1Projective};
+#[cfg(feature = "groups")]
+pub use g2::{G2Affine, G2Projective};
+
+#[cfg(feature = "groups")]
+mod fp12;
+#[cfg(feature = "groups")]
+mod fp6;
+
+// The BLS parameter x for BLS12-381 is -0xd201000000010000
+const BLS_X: u64 = 0xd201000000010000;
+const BLS_X_IS_NEGATIVE: bool = true;
+
+#[cfg(feature = "groups")]
+mod pairings;
+
+pub use pairings::{pairing, Gt, MillerLoopResult};
+
+// TODO: This should be upstreamed to subtle.
+// See https://github.com/dalek-cryptography/subtle/pull/48
+trait CtOptionExt<T> {
+    /// Calls f() and either returns self if it contains a value,
+    /// or returns the output of f() otherwise.
+    fn or_else<F: FnOnce() -> subtle::CtOption<T>>(self, f: F) -> subtle::CtOption<T>;
+}
+
+impl<T: subtle::ConditionallySelectable> CtOptionExt<T> for subtle::CtOption<T> {
+    fn or_else<F: FnOnce() -> subtle::CtOption<T>>(self, f: F) -> subtle::CtOption<T> {
+        let is_none = self.is_none();
+        let f = f();
+
+        subtle::ConditionallySelectable::conditional_select(&self, &f, is_none)
+    }
+}
