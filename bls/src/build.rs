8c960f47bb5314e7411781534dbeba0204c446c9
Michael Straka <mstraka100@gmail.com>
2019-09-30 11:06:43 -0700

    thumbv6 compiles

--- bls/src/build.rs
+++ bls/src/build.rs
@@ -1,9 +1,15 @@
+#[cfg(feature="gen-header")]
 extern crate cbindgen;
 
+#[cfg(feature="gen-header")]
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate C bindings.")
         .write_to_file("./bls-embedded.h");
+}
+
+#[cfg(not(feature="gen-header"))]
+fn main() {
 }
--- bls/src/lib.rs
+++ bls/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(gen_header), no_std)]
 extern crate libc;
 //extern crate rand;
 //extern crate blake2s_simd;
@@ -6,13 +7,17 @@ pub mod bls;
 //pub mod hash;
 pub mod error;
 
-use libc::c_int;
-
 use bls12_377::G2Projective;
 use crate::bls::keys::{PublicKey, PrivateKey, Signature};
 use crate::error::ErrorCode;
 
 use core::slice;
+
+#[cfg(not(gen_header))]
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
 
 fn convert_result_to_bool<T, E, F: Fn() -> Result<T, E>>(f: F) -> bool {
     match f() {
@@ -53,7 +58,7 @@ pub extern "C" fn generate_hash(out_hash: *mut *mut G2Projective) -> bool {
 #[no_mangle]
 pub extern "C" fn deserialize_private_key(
     _in_private_key_bytes: *const u8,
-    _in_private_key_bytes_len: c_int,
+    _in_private_key_bytes_len: i32,
     _out_private_key: *mut *mut PrivateKey,
 ) -> bool {
     unimplemented!();
@@ -63,7 +68,7 @@ pub extern "C" fn deserialize_private_key(
 pub extern "C" fn serialize_private_key(
     _in_private_key: *const PrivateKey,
     _out_bytes: *mut *mut u8,
-    _out_len: *mut c_int,
+    _out_len: *mut i32,
 ) -> bool {
     unimplemented!();
 }
@@ -83,9 +88,9 @@ pub extern "C" fn private_key_to_public_key(
 pub extern "C" fn sign_message(
     in_private_key: *const PrivateKey,
     in_message: *const u8,
-    in_message_len: c_int,
+    in_message_len: i32,
     in_extra_data: *const u8,
-    in_extra_data_len: c_int,
+    in_extra_data_len: i32,
     should_use_composite: bool,
     out_signature: *mut *mut Signature,
     hash: *const G2Projective,
@@ -120,7 +125,7 @@ pub extern "C" fn destroy_private_key(_private_key: *mut PrivateKey) {
 }
 
 #[no_mangle]
-pub extern "C" fn free_vec(_bytes: *mut u8, _len: c_int) {
+pub extern "C" fn free_vec(_bytes: *mut u8, _len: i32) {
     unimplemented!();
 }
 
@@ -137,7 +142,7 @@ pub extern "C" fn destroy_signature(_signature: *mut Signature) {
 #[no_mangle]
 pub extern "C" fn deserialize_public_key(
     _in_public_key_bytes: *const u8,
-    _in_public_key_bytes_len: c_int,
+    _in_public_key_bytes_len: i32,
     _out_public_key: *mut *mut PublicKey,
 ) -> bool {
     unimplemented!();
@@ -147,7 +152,7 @@ pub extern "C" fn deserialize_public_key(
 pub extern "C" fn serialize_public_key(
     _in_public_key: *const PublicKey,
     _out_bytes: *mut *mut u8,
-    _out_len: *mut c_int,
+    _out_len: *mut i32,
 ) -> bool {
     unimplemented!();
 }
@@ -155,7 +160,7 @@ pub extern "C" fn serialize_public_key(
 #[no_mangle]
 pub extern "C" fn deserialize_signature(
     _in_signature_bytes: *const u8,
-    _in_signature_bytes_len: c_int,
+    _in_signature_bytes_len: i32,
     _out_signature: *mut *mut Signature,
 ) -> bool {
     unimplemented!();
@@ -165,7 +170,7 @@ pub extern "C" fn deserialize_signature(
 pub extern "C" fn serialize_signature(
     _in_signature: *const Signature,
     _out_bytes: *mut *mut u8,
-    _out_len: *mut c_int,
+    _out_len: *mut i32,
 ) -> bool {
     unimplemented!();
 }
@@ -174,9 +179,9 @@ pub extern "C" fn serialize_signature(
 pub extern "C" fn verify_signature(
     _in_public_key: *const PublicKey,
     _in_message: *const u8,
-    _in_message_len: c_int,
+    _in_message_len: i32,
     _in_extra_data: *const u8,
-    _in_extra_data_len: c_int,
+    _in_extra_data_len: i32,
     _in_signature: *const Signature,
     _should_use_composite: bool,
     _out_verified: *mut bool,
@@ -196,7 +201,7 @@ pub extern "C" fn verify_pop(
 #[no_mangle]
 pub extern "C" fn aggregate_public_keys(
     _in_public_keys: *const *const PublicKey,
-    _in_public_keys_len: c_int,
+    _in_public_keys_len: i32,
     _out_public_key: *mut *mut PublicKey,
 ) -> bool {
     unimplemented!();
@@ -206,7 +211,7 @@ pub extern "C" fn aggregate_public_keys(
 pub extern "C" fn aggregate_public_keys_subtract(
     _in_aggregated_public_key: *const PublicKey,
     _in_public_keys: *const *const PublicKey,
-    _in_public_keys_len: c_int,
+    _in_public_keys_len: i32,
     _out_public_key: *mut *mut PublicKey,
 ) -> bool {
     unimplemented!();
@@ -215,7 +220,7 @@ pub extern "C" fn aggregate_public_keys_subtract(
 #[no_mangle]
 pub extern "C" fn aggregate_signatures(
     _in_signatures: *const *const Signature,
-    _in_signatures_len: c_int,
+    _in_signatures_len: i32,
     _out_signature: *mut *mut Signature,
 ) -> bool {
     unimplemented!();
