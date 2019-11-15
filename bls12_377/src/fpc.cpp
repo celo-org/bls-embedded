5fc2ef5a54347f13bd2ebfb8c0c1f617d0be25d4
Jason Ansel <jansel@jansel.net>
2019-11-14 23:29:19 -0800

    .

--- bls12_377/src/fpc.cpp
+++ bls12_377/src/fpc.cpp
@@ -265,15 +265,129 @@ void mul12(uint32_t* output, const uint32_t* left, const uint32_t* right) {
 }
 
 inline
+void mul_add32(uint32_t* out,
+               uint32_t* out_carry,
+               uint32_t a,
+               uint32_t b,
+               uint32_t c,
+               uint32_t d) {
+    uint64_t ret = ((uint64_t)a * (uint64_t)b) + (uint64_t)c + (uint64_t)d;
+    *out = (uint64_t)ret;
+    *out_carry = (uint64_t)(ret >> 32);
+}
+
+inline
+void mul_add32(uint32_t* out,
+               uint32_t* out_carry,
+               uint32_t a,
+               uint32_t b,
+               uint32_t c) {
+    uint64_t ret = ((uint64_t)a * (uint64_t)b) + (uint64_t)c;
+    *out = (uint64_t)ret;
+    *out_carry = (uint64_t)(ret >> 32);
+}
+
+inline
+void mul32(uint32_t* out,
+           uint32_t* out_carry,
+           uint32_t a,
+           uint32_t b,
+           uint32_t c) {
+    uint64_t ret = ((uint64_t)a * (uint64_t)b);
+    *out = (uint64_t)ret;
+    *out_carry = (uint64_t)(ret >> 32);
+}
+
+inline
+void add32(uint32_t* out,
+           uint32_t* out_carry,
+           uint32_t a,
+           uint32_t b,
+           uint32_t c) {
+    uint64_t ret = (uint64_t)a + (uint64_t)b + (uint64_t)c;
+    *out = (uint32_t)ret;
+    *out_carry = (uint32_t)(ret >> 32);
+}
+
+inline
+void add32(uint32_t* out,
+           uint32_t* out_carry,
+           uint32_t a,
+           uint32_t b,
+           uint32_t c,
+           uint32_t d) {
+    uint64_t ret = (uint64_t)a + (uint64_t)b + (uint64_t)c + (uint64_t)d;
+    *out = (uint32_t)ret;
+    *out_carry = (uint32_t)(ret >> 32);
+}
+
+
+inline
 void mul_add64(uint64_t* out,
                uint64_t* out_carry,
                uint64_t a,
                uint64_t b,
                uint64_t c,
                uint64_t d) {
+#if __arm__
+    uint32_t a0 = (uint32_t) a;
+    uint32_t a1 = (uint32_t) (a >> 32);
+    uint32_t b0 = (uint32_t) b;
+    uint32_t b1 = (uint32_t) (b >> 32);
+    uint32_t c0 = (uint32_t) c;
+    uint32_t c1 = (uint32_t) (c >> 32);
+    uint32_t d0 = (uint32_t) d;
+    uint32_t d1 = (uint32_t) (d >> 32);
+    uint32_t o0 = 0;
+    uint32_t o1 = 0;
+    uint32_t o2 = 0;
+    uint32_t o3 = 0;
+    uint32_t t1 = 0;
+    uint32_t t2 = 0;
+
+    asm volatile (
+        "UMULL %[o0], %[o1], %[a0], %[b0]\n\t"
+        "UMLAL %[o1], %[o2], %[a0], %[b1]\n\t"
+        "UMLAL %[o2], %[o3], %[a1], %[b1]\n\t"
+
+        "UMULL %[t1], %[t2], %[a1], %[b0]\n\t"
+        "ADDS %[o1], %[o1], %[t1]\n\t"
+        "ADCS %[o2], %[o2], %[t2]\n\t"
+        "ADC  %[o3], %[o3], #0\n\t"
+        
+        "ADDS %[o0], %[o0], %[c0]\n\t"
+        "ADCS %[o1], %[o1], %[c1]\n\t"
+        "ADCS %[o2], %[o2], #0\n\t"
+        "ADC  %[o3], %[o3], #0\n\t"
+
+        "ADDS %[o0], %[o0], %[d0]\n\t"
+        "ADCS %[o1], %[o1], %[d1]\n\t"
+        "ADCS %[o2], %[o2], #0\n\t"
+        "ADC  %[o3], %[o3], #0\n\t"
+
+        : [o0] "+r" (o0),
+          [o1] "+r" (o1),
+          [o2] "+r" (o2),
+          [o3] "+r" (o3),
+          [t1] "+r" (t1),
+          [t2] "+r" (t2)
+        : [a0] "r" (a0),
+          [a1] "r" (a1),
+          [b0] "r" (b0),
+          [b1] "r" (b1),
+          [c0] "r" (c0),
+          [c1] "r" (c1),
+          [d0] "r" (d0),
+          [d1] "r" (d1)
+    );
+
+    *out = ((uint64_t)o1 << 32) + (uint64_t)o0;
+    *out_carry = ((uint64_t)o3 << 32) + (uint64_t)o2;
+#else
     uint128_t ret = ((uint128_t)a * (uint128_t)b) + (uint128_t)c + (uint128_t)d;
     *out = (uint64_t)ret;
     *out_carry = (uint64_t)(ret >> 64);
+#endif
 }
 
 inline
@@ -282,10 +396,25 @@ void add64(uint64_t* out,
            uint64_t a,
            uint64_t b,
            uint64_t c) {
-    uint128_t ret = (uint128_t)a + (uint128_t)b + (uint128_t)c;
-    *out = (uint64_t)ret;
-    *out_carry = (uint64_t)(ret >> 64);
+    uint32_t a0 = (uint32_t) a;
+    uint32_t a1 = (uint32_t) (a >> 32);
+    uint32_t b0 = (uint32_t) b;
+    uint32_t b1 = (uint32_t) (b >> 32);
+    uint32_t c0 = (uint32_t) c;
+    uint32_t c1 = (uint32_t) (c >> 32);
+    uint32_t o0 = 0;
+    uint32_t o1 = 0;
+    uint32_t o2 = 0;
+
+    add32(&o0, &o1, a0, b0, c0);
+    add32(&o1, &o2, o1, a1, b1, c1);
+
+    *out = ((uint64_t)o1 << 32) + (uint64_t)o0;
+    *out_carry = o2;
+    return;
 }
+
+
 
 static const uint64_t inv = 9586122913090633727ull;
 static const uint64_t modulus[6] = {
@@ -297,83 +426,29 @@ static const uint64_t modulus[6] = {
     0x1ae3a4617c510ea,
 };
 
+inline
+void montgomery_step(uint64_t* r, const uint64_t* t, uint64_t a)
+{
+    uint64_t k, carry, _;
+    k = t[0] * inv;
+    mul_add64(&_,  &carry, k, modulus[0], t[0], 0);
+    mul_add64(&r[1], &carry, k, modulus[1], t[1], carry);
+    mul_add64(&r[2], &carry, k, modulus[2], t[2], carry);
+    mul_add64(&r[3], &carry, k, modulus[3], t[3], carry);
+    mul_add64(&r[4], &carry, k, modulus[4], t[4], carry);
+    mul_add64(&r[5], &carry, k, modulus[5], t[5], carry);
+    add64(&r[6], &r[7], a, t[6], carry);
+}
+
 void montgomery_reduce(uint64_t* output, const uint64_t* t) {
-        uint64_t carry, k, _, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11;
-
-        const uint64_t t0 = t[0];
-        const uint64_t t1 = t[1];
-        const uint64_t t2 = t[2];
-        const uint64_t t3 = t[3];
-        const uint64_t t4 = t[4];
-        const uint64_t t5 = t[5];
-        const uint64_t t6 = t[6];
-        const uint64_t t7 = t[7];
-        const uint64_t t8 = t[8];
-        const uint64_t t9 = t[9];
-        const uint64_t t10 = t[10];
-        const uint64_t t11 = t[11];
-
-        k = t[0] * inv;
-        mul_add64(&_,  &carry, k, modulus[0], t0, 0);
-        mul_add64(&r1, &carry, k, modulus[1], t1, carry);
-        mul_add64(&r2, &carry, k, modulus[2], t2, carry);
-        mul_add64(&r3, &carry, k, modulus[3], t3, carry);
-        mul_add64(&r4, &carry, k, modulus[4], t4, carry);
-        mul_add64(&r5, &carry, k, modulus[5], t5, carry);
-        add64(&r6, &r7, t6, 0, carry);
-
-        k = r1 * inv;
-        mul_add64(&_,  &carry, k, modulus[0], r1, 0);
-        mul_add64(&r2, &carry, k, modulus[1], r2, carry);
-        mul_add64(&r3, &carry, k, modulus[2], r3, carry);
-        mul_add64(&r4, &carry, k, modulus[3], r4, carry);
-        mul_add64(&r5, &carry, k, modulus[4], r5, carry);
-        mul_add64(&r6, &carry, k, modulus[5], r6, carry);
-        add64(&r7, &r8, t7, r7, carry);
-
-        k = r2 * inv;
-        mul_add64(&_,  &carry, k, modulus[0], r2, 0);
-        mul_add64(&r3, &carry, k, modulus[1], r3, carry);
-        mul_add64(&r4, &carry, k, modulus[2], r4, carry);
-        mul_add64(&r5, &carry, k, modulus[3], r5, carry);
-        mul_add64(&r6, &carry, k, modulus[4], r6, carry);
-        mul_add64(&r7, &carry, k, modulus[5], r7, carry);
-        add64(&r8, &r9, t8, r8, carry);
-
-        k = r3 * inv;
-        mul_add64(&_,  &carry, k, modulus[0], r3, 0);
-        mul_add64(&r4, &carry, k, modulus[1], r4, carry);
-        mul_add64(&r5, &carry, k, modulus[2], r5, carry);
-        mul_add64(&r6, &carry, k, modulus[3], r6, carry);
-        mul_add64(&r7, &carry, k, modulus[4], r7, carry);
-        mul_add64(&r8, &carry, k, modulus[5], r8, carry);
-        add64(&r9, &r10, t9, r9, carry);
-
-        k = r4 * inv;
-        mul_add64(&_,  &carry, k, modulus[0], r4, 0);
-        mul_add64(&r5, &carry, k, modulus[1], r5, carry);
-        mul_add64(&r6, &carry, k, modulus[2], r6, carry);
-        mul_add64(&r7, &carry, k, modulus[3], r7, carry);
-        mul_add64(&r8, &carry, k, modulus[4], r8, carry);
-        mul_add64(&r9, &carry, k, modulus[5], r9, carry);
-        add64(&r10, &r11, t10, r10, carry);
-
-        k = r5 * inv;
-        mul_add64(&_,   &carry, k, modulus[0], r5, 0);
-        mul_add64(&r6,  &carry, k, modulus[1], r6, carry);
-        mul_add64(&r7,  &carry, k, modulus[2], r7, carry);
-        mul_add64(&r8,  &carry, k, modulus[3], r8, carry);
-        mul_add64(&r9,  &carry, k, modulus[4], r9, carry);
-        mul_add64(&r10, &carry, k, modulus[5], r10, carry);
-        add64(&r11, &_, t11, r11, carry);
-
-        output[0] = r6;
-        output[1] = r7;
-        output[2] = r8;
-        output[3] = r9;
-        output[4] = r10;
-        output[5] = r11;
-    }
+    uint64_t r[12];
+    montgomery_step(r, t, 0);
+    montgomery_step(r + 1, r + 1, t[7]);
+    montgomery_step(r + 2, r + 2, t[8]);
+    montgomery_step(r + 3, r + 3, t[9]);
+    montgomery_step(r + 4, r + 4, t[10]);
+    montgomery_step(output - 1, r + 5, t[11]);
+}
 
 extern "C" void c_mul(uint64_t* output, const uint64_t* left, const uint64_t* right) {
     uint64_t tmp[24];
