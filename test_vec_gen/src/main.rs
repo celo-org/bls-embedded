fc7d98209ea72b3a087c5297e25875053f446279
Michael Straka <mstraka100@gmail.com>
2019-12-11 17:49:43 -0800

    added start of tests to check against bls-zexe

--- test_vec_gen/src/main.rs
+++ test_vec_gen/src/main.rs
@@ -1,5 +1,4 @@
 use bls12_377::{G1Affine, G1Projective, Scalar};
-
 use std::fmt;
 
 struct Array<T> {
@@ -13,6 +12,10 @@ impl<T: fmt::Debug> fmt::Debug for Array<T> {
 }
 
 fn main() {
+    let pk_arr: [u64;4] = [0xb0d221e9b28ee17c, 0x24c102256640b07a, 0xefc2f5fe37f3f98c, 0x0165f07185c6f996];
+    let private_key = unsafe { &Scalar::from_raw(pk_arr) };
+    let pk_bytes = private_key.to_bytes();
+    println!("{:?}", pk_bytes);
     let id = G1Projective::generator();
     let id2 = id * Scalar::from(47);
     let id_des = Array { data: G1Affine::to_uncompressed(&G1Affine::from(id2)) };
